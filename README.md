10b566f17ce3d5488f3c9accae719bddedf9f378
cnhjxruibo <cnhjxruibo@outlook.com>
2025-01-12 14:58:38 +0800

    Improve the function and optimize the accuracy of face recognition

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# VIDAI
-Intelligent Video-Audio Fusion Tool
+<div align="center">
+![WX20250112-145708](/Users/0xm1ng/Downloads/VIDAI/README.assets/WX20250112-145708.png)
+
+  <a href='https://arxiv.org/abs/2211.12194'><img src='https://img.shields.io/badge/ArXiv-PDF-red'></a> &nbsp; <a href='https://sadtalker.github.io'><img src='https://img.shields.io/badge/Project-Page-Green'></a> [![Hugging Face Spaces](https://img.shields.io/badge/%F0%9F%A4%97%20Hugging%20Face-Spaces-blue)](https://huggingface.co/spaces/vinthony/SadTalker) &nbsp; [![sd webui-colab](https://img.shields.io/badge/Automatic1111-Colab-green)](https://colab.research.google.com/github/camenduru/stable-diffusion-webui-colab/blob/main/video/stable/stable_diffusion_1_5_video_webui_colab.ipynb)  [![Replicate](https://replicate.com/cjwbw/sadtalker/badge)](https://replicate.com/cjwbw/sadtalker) 
+
+
+![sadtalker](https://user-images.githubusercontent.com/4397546/222490039-b1f6156b-bf00-405b-9fda-0c9a9156f991.gif)
+
+
+
+## About US
+
+> VIDAI: Intelligent Video-Audio Fusion Tool
+
+### Versatile Applications
+
+Perfect for film production, educational training, and social media content creation.
+
+### Effortless Use
+
+##### User-friendly interface allows anyone to generate high-quality results with ease.
+
+### Multi-Language Support
+
+Enables cross-language audio synchronization, making content globally accessible.
+
+![image-20250112145215799](/Users/0xm1ng/Downloads/VIDAI/README.assets/image-20250112145215799.png)
+
+## To-Do
+
+We're tracking new updates in [issue #280](https://github.com/OpenTalker/SadTalker/issues/280).
+
+## Troubleshooting
+
+If you have any problems, please read our [FAQs](docs/FAQ.md) before opening an issue.
+
+## 1. Installation.
+
+Community tutorials: [中文Windows教程 (Chinese Windows tutorial)](https://www.bilibili.com/video/BV1Dc411W7V6/) | [日本語コース (Japanese tutorial)](https://br-d.fanbox.cc/posts/5685086).
+
+### Linux/Unix
+
+1. Install [Anaconda](https://www.anaconda.com/), Python and `git`.
+
+2. Creating the env and install the requirements.
+  ```bash
+  git clone https://github.com/OpenTalker/SadTalker.git
+
+  cd SadTalker 
+
+  conda create -n sadtalker python=3.8
+
+  conda activate sadtalker
+
+  pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+
+  conda install ffmpeg
+
+  pip install -r requirements.txt
+
+  ### Coqui TTS is optional for gradio demo. 
+  ### pip install TTS
+
+  ```
+### Windows
+
+A video tutorial in chinese is available [here](https://www.bilibili.com/video/BV1Dc411W7V6/). You can also follow the following instructions:
+
+1. Install [Python 3.8](https://www.python.org/downloads/windows/) and check "Add Python to PATH".
+2. Install [git](https://git-scm.com/download/win) manually or using [Scoop](https://scoop.sh/): `scoop install git`.
+3. Install `ffmpeg`, following [this tutorial](https://www.wikihow.com/Install-FFmpeg-on-Windows) or using [scoop](https://scoop.sh/): `scoop install ffmpeg`.
+4. Download the SadTalker repository by running `git clone https://github.com/Winfredy/SadTalker.git`.
+5. Download the checkpoints and gfpgan models in the [downloads section](#2-download-models).
+6. Run `start.bat` from Windows Explorer as normal, non-administrator, user, and a Gradio-powered WebUI demo will be started.
+
+### macOS
+
+A tutorial on installing SadTalker on macOS can be found [here](docs/install.md).
+
+### Docker, WSL, etc
+
+Please check out additional tutorials [here](docs/install.md).
--- inference.py
+++ inference.py
@@ -1,145 +1,150 @@
-from glob import glob
+import os
+import sys
 import shutil
 import torch
-from time import  strftime
-import os, sys, time
+from time import strftime
 from argparse import ArgumentParser
+from glob import glob
 
 from src.utils.preprocess import CropAndExtract
-from src.test_audio2coeff import Audio2Coeff  
+from src.test_audio2coeff import Audio2Coeff
 from src.facerender.animate import AnimateFromCoeff
 from src.generate_batch import get_data
 from src.generate_facerender_batch import get_facerender_data
 from src.utils.init_path import init_path
 
+
+def safe_mkdir(directory):
+    """Create a directory if it does not exist."""
+    os.makedirs(directory, exist_ok=True)
+
+
+def validate_paths(paths):
+    """Validate that required paths exist."""
+    for path in paths:
+        if path and not os.path.exists(path):
+            raise FileNotFoundError(f"Path does not exist: {path}")
+
+
+def setup_logging(log_dir):
+    """Setup logging directory."""
+    safe_mkdir(log_dir)
+    log_file = os.path.join(log_dir, 'process.log')
+    sys.stdout = open(log_file, 'w')
+    sys.stderr = sys.stdout
+
+
 def main(args):
-    #torch.backends.cudnn.enabled = False
-
-    pic_path = args.source_image
-    audio_path = args.driven_audio
+    # Logging setup
     save_dir = os.path.join(args.result_dir, strftime("%Y_%m_%d_%H.%M.%S"))
-    os.makedirs(save_dir, exist_ok=True)
-    pose_style = args.pose_style
-    device = args.device
-    batch_size = args.batch_size
-    input_yaw_list = args.input_yaw
-    input_pitch_list = args.input_pitch
-    input_roll_list = args.input_roll
-    ref_eyeblink = args.ref_eyeblink
-    ref_pose = args.ref_pose
+    setup_logging(save_dir)
 
-    current_root_path = os.path.split(sys.argv[0])[0]
+    # Validate input paths
+    validate_paths([args.source_image, args.driven_audio, args.checkpoint_dir])
 
-    sadtalker_paths = init_path(args.checkpoint_dir, os.path.join(current_root_path, 'src/config'), args.size, args.old_version, args.preprocess)
-
-    #init model
-    preprocess_model = CropAndExtract(sadtalker_paths, device)
-
-    audio_to_coeff = Audio2Coeff(sadtalker_paths,  device)
-    
-    animate_from_coeff = AnimateFromCoeff(sadtalker_paths, device)
-
-    #crop image and extract 3dmm from image
+    # Prepare directories
     first_frame_dir = os.path.join(save_dir, 'first_frame_dir')
-    os.makedirs(first_frame_dir, exist_ok=True)
-    print('3DMM Extraction for source image')
-    first_coeff_path, crop_pic_path, crop_info =  preprocess_model.generate(pic_path, first_frame_dir, args.preprocess,\
-                                                                             source_image_flag=True, pic_size=args.size)
+    safe_mkdir(first_frame_dir)
+
+    print("Initializing paths and models...")
+    sadtalker_paths = init_path(args.checkpoint_dir, os.path.join(os.path.dirname(__file__), 'src/config'),
+                                args.size, args.old_version, args.preprocess)
+
+    preprocess_model = CropAndExtract(sadtalker_paths, args.device)
+    audio_to_coeff = Audio2Coeff(sadtalker_paths, args.device)
+    animate_from_coeff = AnimateFromCoeff(sadtalker_paths, args.device)
+
+    # 3DMM Extraction for source image
+    print("Extracting 3DMM for source image...")
+    first_coeff_path, crop_pic_path, crop_info = preprocess_model.generate(
+        args.source_image, first_frame_dir, args.preprocess, source_image_flag=True, pic_size=args.size
+    )
+
     if first_coeff_path is None:
-        print("Can't get the coeffs of the input")
+        print("Error: Unable to extract coefficients from the source image.")
         return
 
-    if ref_eyeblink is not None:
-        ref_eyeblink_videoname = os.path.splitext(os.path.split(ref_eyeblink)[-1])[0]
-        ref_eyeblink_frame_dir = os.path.join(save_dir, ref_eyeblink_videoname)
-        os.makedirs(ref_eyeblink_frame_dir, exist_ok=True)
-        print('3DMM Extraction for the reference video providing eye blinking')
-        ref_eyeblink_coeff_path, _, _ =  preprocess_model.generate(ref_eyeblink, ref_eyeblink_frame_dir, args.preprocess, source_image_flag=False)
-    else:
-        ref_eyeblink_coeff_path=None
+    # Process reference videos for eye blinking and pose
+    ref_eyeblink_coeff_path = None
+    if args.ref_eyeblink:
+        ref_eyeblink_dir = os.path.join(save_dir, 'ref_eyeblink')
+        safe_mkdir(ref_eyeblink_dir)
+        print("Extracting 3DMM for reference video (eye blinking)...")
+        ref_eyeblink_coeff_path, _, _ = preprocess_model.generate(
+            args.ref_eyeblink, ref_eyeblink_dir, args.preprocess, source_image_flag=False
+        )
 
-    if ref_pose is not None:
-        if ref_pose == ref_eyeblink: 
-            ref_pose_coeff_path = ref_eyeblink_coeff_path
-        else:
-            ref_pose_videoname = os.path.splitext(os.path.split(ref_pose)[-1])[0]
-            ref_pose_frame_dir = os.path.join(save_dir, ref_pose_videoname)
-            os.makedirs(ref_pose_frame_dir, exist_ok=True)
-            print('3DMM Extraction for the reference video providing pose')
-            ref_pose_coeff_path, _, _ =  preprocess_model.generate(ref_pose, ref_pose_frame_dir, args.preprocess, source_image_flag=False)
-    else:
-        ref_pose_coeff_path=None
+    ref_pose_coeff_path = None
+    if args.ref_pose:
+        ref_pose_dir = os.path.join(save_dir, 'ref_pose')
+        safe_mkdir(ref_pose_dir)
+        print("Extracting 3DMM for reference video (pose)...")
+        ref_pose_coeff_path, _, _ = preprocess_model.generate(
+            args.ref_pose, ref_pose_dir, args.preprocess, source_image_flag=False
+        )
 
-    #audio2ceoff
-    batch = get_data(first_coeff_path, audio_path, device, ref_eyeblink_coeff_path, still=args.still)
-    coeff_path = audio_to_coeff.generate(batch, save_dir, pose_style, ref_pose_coeff_path)
+    # Generate coefficients from audio
+    print("Generating coefficients from audio...")
+    batch = get_data(first_coeff_path, args.driven_audio, args.device, ref_eyeblink_coeff_path, still=args.still)
+    coeff_path = audio_to_coeff.generate(batch, save_dir, args.pose_style, ref_pose_coeff_path)
 
-    # 3dface render
+    # Generate 3D face visualization (optional)
     if args.face3dvis:
         from src.face3d.visualize import gen_composed_video
-        gen_composed_video(args, device, first_coeff_path, coeff_path, audio_path, os.path.join(save_dir, '3dface.mp4'))
-    
-    #coeff2video
-    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, audio_path, 
-                                batch_size, input_yaw_list, input_pitch_list, input_roll_list,
-                                expression_scale=args.expression_scale, still_mode=args.still, preprocess=args.preprocess, size=args.size)
-    
-    result = animate_from_coeff.generate(data, save_dir, pic_path, crop_info, \
-                                enhancer=args.enhancer, background_enhancer=args.background_enhancer, preprocess=args.preprocess, img_size=args.size)
-    
-    shutil.move(result, save_dir+'.mp4')
-    print('The generated video is named:', save_dir+'.mp4')
+        print("Generating 3D face visualization...")
+        gen_composed_video(args, args.device, first_coeff_path, coeff_path, args.driven_audio,
+                           os.path.join(save_dir, '3dface.mp4'))
 
+    # Generate the final video
+    print("Generating final animated video...")
+    data = get_facerender_data(coeff_path, crop_pic_path, first_coeff_path, args.driven_audio,
+                               args.batch_size, args.input_yaw, args.input_pitch, args.input_roll,
+                               expression_scale=args.expression_scale, still_mode=args.still,
+                               preprocess=args.preprocess, size=args.size)
+
+    result_video = animate_from_coeff.generate(
+        data, save_dir, args.source_image, crop_info, enhancer=args.enhancer,
+        background_enhancer=args.background_enhancer, preprocess=args.preprocess, img_size=args.size
+    )
+
+    shutil.move(result_video, f"{save_dir}.mp4")
+    print(f"The generated video is saved at: {save_dir}.mp4")
+
+    # Clean up intermediate files if verbose mode is off
     if not args.verbose:
+        print("Cleaning up intermediate files...")
         shutil.rmtree(save_dir)
 
-    
+
 if __name__ == '__main__':
-
-    parser = ArgumentParser()  
-    parser.add_argument("--driven_audio", default='./examples/driven_audio/bus_chinese.wav', help="path to driven audio")
-    parser.add_argument("--source_image", default='./examples/source_image/full_body_1.png', help="path to source image")
-    parser.add_argument("--ref_eyeblink", default=None, help="path to reference video providing eye blinking")
-    parser.add_argument("--ref_pose", default=None, help="path to reference video providing pose")
-    parser.add_argument("--checkpoint_dir", default='./checkpoints', help="path to output")
-    parser.add_argument("--result_dir", default='./results', help="path to output")
-    parser.add_argument("--pose_style", type=int, default=0,  help="input pose style from [0, 46)")
-    parser.add_argument("--batch_size", type=int, default=2,  help="the batch size of facerender")
-    parser.add_argument("--size", type=int, default=256,  help="the image size of the facerender")
-    parser.add_argument("--expression_scale", type=float, default=1.,  help="the batch size of facerender")
-    parser.add_argument('--input_yaw', nargs='+', type=int, default=None, help="the input yaw degree of the user ")
-    parser.add_argument('--input_pitch', nargs='+', type=int, default=None, help="the input pitch degree of the user")
-    parser.add_argument('--input_roll', nargs='+', type=int, default=None, help="the input roll degree of the user")
-    parser.add_argument('--enhancer',  type=str, default=None, help="Face enhancer, [gfpgan, RestoreFormer]")
-    parser.add_argument('--background_enhancer',  type=str, default=None, help="background enhancer, [realesrgan]")
-    parser.add_argument("--cpu", dest="cpu", action="store_true") 
-    parser.add_argument("--face3dvis", action="store_true", help="generate 3d face and 3d landmarks") 
-    parser.add_argument("--still", action="store_true", help="can crop back to the original videos for the full body aniamtion") 
-    parser.add_argument("--preprocess", default='crop', choices=['crop', 'extcrop', 'resize', 'full', 'extfull'], help="how to preprocess the images" ) 
-    parser.add_argument("--verbose",action="store_true", help="saving the intermedia output or not" ) 
-    parser.add_argument("--old_version",action="store_true", help="use the pth other than safetensor version" ) 
-
-
-    # net structure and parameters
-    parser.add_argument('--net_recon', type=str, default='resnet50', choices=['resnet18', 'resnet34', 'resnet50'], help='useless')
-    parser.add_argument('--init_path', type=str, default=None, help='Useless')
-    parser.add_argument('--use_last_fc',default=False, help='zero initialize the last fc')
-    parser.add_argument('--bfm_folder', type=str, default='./checkpoints/BFM_Fitting/')
-    parser.add_argument('--bfm_model', type=str, default='BFM_model_front.mat', help='bfm model')
-
-    # default renderer parameters
-    parser.add_argument('--focal', type=float, default=1015.)
-    parser.add_argument('--center', type=float, default=112.)
-    parser.add_argument('--camera_d', type=float, default=10.)
-    parser.add_argument('--z_near', type=float, default=5.)
-    parser.add_argument('--z_far', type=float, default=15.)
+    parser = ArgumentParser()
+    parser.add_argument("--driven_audio", required=True, help="Path to driven audio")
+    parser.add_argument("--source_image", required=True, help="Path to source image")
+    parser.add_argument("--ref_eyeblink", default=None, help="Path to reference video for eye blinking")
+    parser.add_argument("--ref_pose", default=None, help="Path to reference video for pose")
+    parser.add_argument("--checkpoint_dir", required=True, help="Path to model checkpoints")
+    parser.add_argument("--result_dir", default="./results", help="Path to save results")
+    parser.add_argument("--pose_style", type=int, default=0, help="Pose style index")
+    parser.add_argument("--batch_size", type=int, default=2, help="Batch size for face rendering")
+    parser.add_argument("--size", type=int, default=256, help="Image size for processing")
+    parser.add_argument("--expression_scale", type=float, default=1.0, help="Expression intensity scale")
+    parser.add_argument('--input_yaw', nargs='+', type=int, default=None, help="Input yaw angles")
+    parser.add_argument('--input_pitch', nargs='+', type=int, default=None, help="Input pitch angles")
+    parser.add_argument('--input_roll', nargs='+', type=int, default=None, help="Input roll angles")
+    parser.add_argument('--enhancer', type=str, default=None, help="Face enhancer (e.g., gfpgan)")
+    parser.add_argument('--background_enhancer', type=str, default=None, help="Background enhancer (e.g., realesrgan)")
+    parser.add_argument("--cpu", action="store_true", help="Use CPU instead of GPU")
+    parser.add_argument("--face3dvis", action="store_true", help="Generate 3D face visualization")
+    parser.add_argument("--still", action="store_true", help="Enable still mode for full body animation")
+    parser.add_argument("--preprocess", default='crop', choices=['crop', 'extcrop', 'resize', 'full', 'extfull'],
+                        help="Preprocessing method")
+    parser.add_argument("--verbose", action="store_true", help="Keep intermediate files")
+    parser.add_argument("--old_version", action="store_true", help="Use old checkpoint version")
 
     args = parser.parse_args()
 
-    if torch.cuda.is_available() and not args.cpu:
-        args.device = "cuda"
-    else:
-        args.device = "cpu"
+    # Set device
+    args.device = "cuda" if torch.cuda.is_available() and not args.cpu else "cpu"
 
+    # Run main process
     main(args)
-
